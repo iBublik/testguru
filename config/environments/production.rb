a7bf9bb7a16c59ea578bd720bfe54662d4ac25ce
yuneszadi <yuneszadi@gmail.com>
2018-09-24 13:34:16 +0300

    xxxyyzzzz

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
       user_name:            ENV['SMTP_USERNAME'],
       password:             ENV['SMTP_PASSWORD'],
       authentication:       'plain',
+      domain:               'gmail.com',
       enable_starttls_auto: true
     }
 
