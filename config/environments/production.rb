53c40c6c099e9c3004a6baf83af639223eb3814a
yuneszadi <yuneszadi@gmail.com>
2018-09-25 19:38:20 +0300

    xxxyyzzzzo98

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      user_name:            'yuneszadi@gmail.com ',
+      user_name:            ENV['SMTP_USERNAME'],
       password:             ENV['SMTP_PASSWORD'],
       authentication:       'plain',
       enable_starttls_auto: true
